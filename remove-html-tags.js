56ec53051834eff40c82fdd5021eed8f50acd920
miggy92 <m92fuentes@gmail.com>
2019-12-12 16:38:34 -0500

    remove ng-atributes and fixed traverse

--- remove-html-tags.js
+++ remove-html-tags.js
@@ -1,11 +1,11 @@
 const csstree = require('css-tree');
 const {minify} = require('./src/htmlminifier');
+const {range} = require('lodash');
 
 const test = (text) => {
   const domParser = new DOMParser();
   const parsed = domParser.parseFromString(text,"text/html");
   traverse(null, parsed);
-  console.log(parsed.documentElement.outerHTML);
 
   return minify(parsed.documentElement.outerHTML, {
     collapseBooleanAttributes: true,
@@ -33,12 +33,33 @@ function removeNode(node) {
   if(node instanceof HTMLStyleElement)
     removeCSSComments(node)
 }
+const ngTest = /^((ng)|(rj))-/;
+function removeNgAttributes(node) {
+  const attributes = node.attributes;
+  if(!attributes)
+    return;
+
+  range(attributes.length)
+    .filter(i => {
+        return ngTest.test(attributes[i].name)
+      })
+    .map(i => attributes[i].name)
+    .forEach(name => attributes.removeNamedItem(name))
+}
 function traverse(parentNode, node){
+  if(node.visited)
+    return
+  node.visited = true;
   if(removeNode(node))
     return parentNode.removeChild(node);
 
+  removeNgAttributes(node);
+
+  if(node.children)
+    for( let childNode of node.children)
+      traverse(node, childNode);
   for( let childNode of node.childNodes)
     traverse(node, childNode);
 }
 
-global.test = test
+global.test = test;
